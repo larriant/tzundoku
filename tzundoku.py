99a588ce14295d2a067bb3b618fe89a5f6f3c0c4
Julyan Davey <me@julyandavey.com>
2015-03-30 23:20:37 +0100

    Login and Register Templates added

--- tzundoku.py
+++ tzundoku.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html') 
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/logout")
+def logout():
+    return render_template('logout.html')
+
 if __name__ == "__main__":
     app.run()
 
